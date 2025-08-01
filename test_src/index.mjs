c0230a22cde745c66beb3a90706ce5ae3efc76d1
Atreya Bain <atreyabain@gmail.com>
2025-08-01 20:49:08 +0100

    [fix] test

--- test_src/index.mjs
+++ test_src/index.mjs
@@ -2,11 +2,11 @@ import fs,{readFile} from 'node:fs'
 
 import classnames from 'classnames'
 // import {neq} from 'semver'
-import {sum, div,sad} from './arithmetic.cjs';
+import {sum, div} from './arithmetic.cjs';
 
 readFile('a',(err)=>{
     if(err){return;}
 })
-console.log(classnames('a', 'b', 'c',{a:5})); // $ExpectType string
+console.log(classnames({a:true,b:true})); // $ExpectType string
 console.log(sum(2, 3)); 
 // console.log(neq('1.0.0', '1.0.1')); // $ExpectType boolean
